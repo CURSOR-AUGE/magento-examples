b15afa1ce55ba8b1114f07e0df07ec619b9fc41e
Your Name <you@example.com>
2020-01-02 19:27:50 +0100

    rewrite

---
+++ Example/ModuleName/Rewrite/Magento/Catalog/Block/Product/View.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Example\ModuleName\Rewrite\Magento\Catalog\Block\Product;
+
+
+class View extends \Magento\Catalog\Block\Product\View
+{
+    /**
+     * Retrieve current product model
+     *
+     * @return \Magento\Catalog\Model\Product
+     */
+    public function getProduct()
+    {
+        print_r('rxxxxxxxxxxxxxxxxxxxxxxxxxxx');die();
+        if (!$this->_coreRegistry->registry('product') && $this->getProductId()) {
+            $product = $this->productRepository->getById($this->getProductId());
+            $this->_coreRegistry->register('product', $product);
+        }
+        return $this->_coreRegistry->registry('product');
+    }
+
+}
