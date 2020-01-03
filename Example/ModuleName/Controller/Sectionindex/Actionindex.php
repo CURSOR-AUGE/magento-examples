e37360034efd21d162665e42ae38b757902beae5
Your Name <you@example.com>
2020-01-04 00:27:35 +0100

    rewrite

--- Example/ModuleName/Controller/Sectionindex/Actionindex.php
+++ Example/ModuleName/Controller/Sectionindex/Actionindex.php
@@ -7,7 +7,7 @@ class Actionindex extends \Magento\Framework\App\Action\Action
 {
 
     protected $resultPageFactory;
-
+    protected $customerSession;
     /**
      * Constructor
      *
@@ -17,8 +17,10 @@ class Actionindex extends \Magento\Framework\App\Action\Action
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\View\Result\PageFactory $resultPageFactory,
-        \Magento\Customer\Model\Customer $customers
+        \Magento\Customer\Model\Customer $customers,
+        \Magento\Customer\Model\Session $customerSession
     ) {
+        $this->customerSession = $customerSession;
         $this->resultPageFactory = $resultPageFactory;
         parent::__construct($context);
     }
@@ -35,5 +37,16 @@ class Actionindex extends \Magento\Framework\App\Action\Action
     public function execute()
     {
         return $this->resultPageFactory->create();
+    }
+
+    public function getCustomerDeliveryPostcode() {
+        $customer = $this->customerSession->getCustomer();
+        if ($customer) {
+            $shippingAddress = $customer->getDefaultShippingAddress();
+            if ($shippingAddress) {
+                return $shippingAddress->getPostcode();
+            }
+        }
+        return null;
     }
 }
